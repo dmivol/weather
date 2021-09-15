315ec5228799170622bd6ea865d42dd3067fbefd
dmivol <dmivol777@gmail.com>
2021-09-15 21:35:45 +0300

    Description commit

--- main.dart
+++ main.dart
@@ -2,23 +2,34 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 import './models/weather.dart';
+
 // todo В функции weather сделать возможность передачи функции города
-void main()async{
-  // for (var i = 0; i < 1; i++) {
-  //   final result = await getInput();
-  //   print('Ваш город - это $result');
-  //   if (result.isEmpty) {
-  //     print('Вы не вввели название города');
-  //   }
-  // }
-  getWeather('Kharkov');
+void main() async {
+  getWeather() {
+  }
+  }
+getWeather() {
+  for (var i = 0; i < 1; i++) {
+    final city = getInput();
+    print('Ваш город - это $city');
+    if (city.isEmpty) {
+      print('Вы не вввели название города');
+    }
+  }
+}
+
+
+
+    }
+  }
 }
 
 Future<String> getInput() async {
   print('Введите название города:');
   return stdin.readLineSync();
-  }
-Future<String> getWeather(String city) async{
+}
+
+Future<String> getWeather(String city) async {
   var url = Uri.parse(
       'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=459f6c565802e5a2171df04458b9a193');
   var response = await http.get(url);
@@ -27,19 +38,16 @@ Future<String> getWeather(String city) async{
 
   if (response.statusCode == 200) {
     var jsonResponse =
-    convert.jsonDecode(response.body) as Map<String, dynamic>;
-   var weather = Weather.fromJson(jsonResponse);
-   print(weather);
-   print(weather.lon);
-   print(weather.lat);
-   print(weather.humidity);
-   print(weather.country);
-   print(weather.speed);
-   print(weather.description);
-   print(weather.all);
-
-
-
+        convert.jsonDecode(response.body) as Map<String, dynamic>;
+    var weather = Weather.fromJson(jsonResponse);
+    print(weather);
+    print(weather.lon);
+    print(weather.lat);
+    print(weather.humidity);
+    print(weather.country);
+    print(weather.speed);
+    print(weather.description);
+    print(weather.all);
   } else {
     print('Request failed with status: ${response.statusCode}.');
   }
